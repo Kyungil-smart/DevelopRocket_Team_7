e19752a5eefca7a1e50a212c6e7f834dc81746d6
jackdoe13 <sejin1489@gmail.com>
2026-03-24 16:23:35 +0900

    refactor : 중복 코드 제거
    refactor : 중복 코드 제거

--- ProjectP/Assets/02.Scripts/Player/PlayerController.cs
+++ ProjectP/Assets/02.Scripts/Player/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
    [SerializeField] private InputActionReference _inputActionReference;
    [SerializeField] private InputActionAsset _inputActionAsset;
 
-   private Vector2 input;
+   [SerializeField] private Vector2 input;
    [SerializeField] private float moveSpeed = 10f;  // 플레이어 기본 속도
    
    private void Update()
@@ -26,7 +26,5 @@ public class PlayerController : MonoBehaviour
    public void Move(InputAction.CallbackContext context)
    {
       input = context.ReadValue<Vector2>();
-      input = new Vector2(input.x, input.y);
-      
    }
 }
