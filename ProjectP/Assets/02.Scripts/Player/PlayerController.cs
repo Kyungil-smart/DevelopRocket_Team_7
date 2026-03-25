1fb85a8faf6573922e9e46fd2a2795c2507713c6
jackdoe13 <sejin1489@gmail.com>
2026-03-25 16:32:31 +0900

    chore : 임시저장
    chore : 임시저장

--- ProjectP/Assets/02.Scripts/Player/PlayerController.cs
+++ ProjectP/Assets/02.Scripts/Player/PlayerController.cs
@@ -11,20 +11,30 @@ public class PlayerController : MonoBehaviour
    [SerializeField] private Vector2 input;
    [SerializeField] private float moveSpeed = 10f;  // 플레이어 기본 속도
    
-   private void Update()
+   [SerializeField] private float dashSpeed = 30f;  // 플레이어 대쉬 속도
+   
+   private void FixedUpdate()
    {
-      transform.Translate(input.normalized * moveSpeed* Time.deltaTime);
+      //transform.Translate(input.normalized * moveSpeed* Time.deltaTime);
+      GetComponent<Rigidbody2D>().linearVelocity = new Vector2(input.x * moveSpeed, input.y * moveSpeed);
    }
 
    private void Awake()
    {
       _inputActionAsset.Enable();
       _inputActionAsset["Move"].performed += Move;
+      _inputActionAsset["Dash"].performed += Dash;
    }
 
 
    public void Move(InputAction.CallbackContext context)
    {
       input = context.ReadValue<Vector2>();
+   }
+   
+   public void Dash(InputAction.CallbackContext context)
+   {
+      input = context.ReadValue<Vector2>();
+      moveSpeed = dashSpeed;
    }
 }
