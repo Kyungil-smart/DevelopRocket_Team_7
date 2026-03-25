29f6d7631def3f443c070d78f12c3e50e64019d8
Byounghee Chae <mybeang@naver.com>
2026-03-25 11:39:59 +0900

    feat: EnemySpawner 소환 함수 파라매터 추가

--- ProjectP/Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ ProjectP/Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -22,13 +22,66 @@ public class EnemySpawner : MonoBehaviour
                 objectDict[prefab.name] = new Queue<GameObject>();
             }
 
-            GameObject obj = Instantiate(prefab);
-            obj.SetActive(false);
-            objectDict[prefab.name].Enqueue(obj);
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject obj = Instantiate(prefab, gameObject.transform, true);
+                obj.name = prefab.name;
+                obj.SetActive(false);
+                objectDict[prefab.name].Enqueue(obj);    
+            }
         }
     }
+
+    /// <summary>
+    /// 일반 몬스터 스폰 함수. 
+    /// </summary>
+    /// <param name="spawnNums">몬스터별 스폰 개수</param>
+    /// <param name="positions">몬스터 스폰 위치 리스트</param>
+    /// <returns></returns>
+    public List<GameObject> Spwan(Dictionary<string, int> spawnNums, List<Vector2> positions)
+    {
+        List<GameObject> spawnedEnemies = new List<GameObject>();
+        Queue<Vector2> spawnedPositions = new Queue<Vector2>(positions);
+        foreach (var spawnNum in spawnNums)
+        {
+            string name = spawnNum.Key;
+            int num = spawnNum.Value;
+            for (int i = 0; i < num; i++)
+            {
+                spawnedEnemies.Add(SpawnEach(name, spawnedPositions.Dequeue()));
+            }
+        }
+        return spawnedEnemies;
+    }
     
-    public GameObject Spawn(string name, Vector2 position)
+    /// <summary>
+    /// 일반 몬스터 스폰 함수. 한 장소에만 스폰 시킬 경우
+    /// </summary>
+    /// <param name="spawnNums">몬스터별 스폰 개수</param>
+    /// <param name="position">몬스터 스폰 위치</param>
+    /// <returns></returns>
+    public List<GameObject> Spwan(Dictionary<string, int> spawnNums, Vector2 position)
+    {
+        List<GameObject> spawnedEnemies = new List<GameObject>();
+        foreach (var spawnNum in spawnNums)
+        {
+            string name = spawnNum.Key;
+            int num = spawnNum.Value;
+            for (int i = 0; i < num; i++)
+            {
+                spawnedEnemies.Add(SpawnEach(name, position));
+            }
+        }
+        return spawnedEnemies;
+    }
+    
+    /// <summary>
+    /// 단일 몬스터 스폰 함수
+    /// </summary>
+    /// <param name="name">Prefab name</param>
+    /// <param name="position">스폰 위치</param>
+    /// <returns></returns>
+    public GameObject SpawnEach(string name, Vector2 position)
     {
         GameObject obj = objectDict[name].Dequeue();
         obj.transform.position = position;
