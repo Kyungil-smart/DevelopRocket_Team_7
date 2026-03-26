b7b8e23613605f507256145ba2b5cab607547edd
jackdoe13 <sejin1489@gmail.com>
2026-03-26 12:02:26 +0900

    feat : 대쉬 쿨타임 기능 추가
    feat : 대쉬 쿨타임 기능 추가
    조절 가능 현재 쿨타임 5초로 설정

--- ProjectP/Assets/02.Scripts/Player/PlayerController.cs
+++ ProjectP/Assets/02.Scripts/Player/PlayerController.cs
@@ -14,15 +14,17 @@ public class PlayerController : Singleton<PlayerController>
    
    [SerializeField] private float dashSpeed = 30f;  // 플레이어 대쉬 속도
    [SerializeField] private float dashTime = 0.5f;  // 플레이어 대쉬 시간
+   [SerializeField] private float dashCooldown = 0f; // 대쉬 쿨타임 
    
-   private bool isDashing = false;  // 플레이어 대시 중인지 체크
+   private bool isDashing = false;  // 플레이어 대쉬 중인지 체크
    
    
    
    
    private void FixedUpdate()
    {
-      if (isDashing) return;
+      if (dashCooldown > 0f) dashCooldown -= Time.fixedDeltaTime; // 대쉬 쿨타임 감소
+      if (isDashing) return;  // 대쉬중이면 일반 이동 불가
       GetComponent<Rigidbody2D>().linearVelocity = new Vector2(input.x * moveSpeed, input.y * moveSpeed);
    }
 
@@ -50,19 +52,22 @@ public class PlayerController : Singleton<PlayerController>
 
    private System.Collections.IEnumerator DashRoutine()
    {
-      
-      isDashing = true;
 
-      Vector2 dashDirection = input.normalized;    // 바라보는 방향으로 대쉬
-      if (dashDirection == Vector2.zero) dashDirection = Vector2.right; // 입력한 키 없으면 기본 우측으로 대쉬
-      
-      GetComponent<Rigidbody2D>().linearVelocity = dashDirection * dashSpeed;
-      //GetComponent<Rigidbody2D>().AddForce(dashDirection * dashSpeed);
-      yield return new WaitForSeconds(dashTime);
-      
-      
-      isDashing = false; // 대쉬 중 종료
-      GetComponent<Rigidbody2D>().linearVelocity =  Vector2.zero;
-      
+      if (dashCooldown <= 0f)    // 대쉬 쿨타임 0초 이하일 때
+      {
+         isDashing = true;
+         dashCooldown = 5f;
+
+         Vector2 dashDirection = input.normalized; // 바라보는 방향으로 대쉬
+         if (dashDirection == Vector2.zero) dashDirection = Vector2.right; // 입력한 키 없으면 기본 우측으로 대쉬
+
+         GetComponent<Rigidbody2D>().linearVelocity = dashDirection * dashSpeed;
+         yield return new WaitForSeconds(dashTime);
+
+
+         isDashing = false; // 대쉬 중 종료
+         GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+      }
+
    }
 }
