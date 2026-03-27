9d2e32e73db0bb51d38a7080217d10fa50289d09
jogyemin <jogyemin5100@gmail.com>
2026-03-27 15:46:49 +0900

    feat: 무기 테스트 기능 구현
    임시 프리팹 및 스크립트 작성

---
+++ ProjectP/Assets/02.Scripts/Weapon/Weapon.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // 구현 원리 요약:
+    // 프리팹 하나가 무기 1개이며 SO 데이터를 참조해서 공격한다
+
+    [Header("무기 데이터")]
+
+    [Tooltip("무기 스탯 데이터")]
+    [SerializeField] private WeaponDataSO weaponData;
+
+    [Tooltip("발사 위치")]
+    [SerializeField] private Transform firePoint;
+
+    private float lastAttackTime;
+
+    public void Init(WeaponDataSO data)
+    {
+        weaponData = data;
+    }
+
+    public void TryAttack()
+    {
+        // 공격 속도 제한
+        if (Time.time - lastAttackTime < 1f / weaponData.attackSpeed)
+            return;
+
+        lastAttackTime = Time.time;
+
+        // 실제 공격 실행
+        weaponData.fireStrategy.Fire(firePoint, weaponData);
+    }
+}
