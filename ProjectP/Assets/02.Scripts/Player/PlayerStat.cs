3bf0b8e0deb26ba159507014341aad5309cd9dd8
jackdoe13 <sejin1489@gmail.com>
2026-03-30 11:32:38 +0900

    feat: 플레이어 스탯 추가
    feat: 플레이어 스탯 추가

---
+++ ProjectP/Assets/02.Scripts/Player/PlayerStat.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    [SerializeField] public int playerHp = 5; // 플레이어 HP
+    [SerializeField] public float moveSpeed = 10f;  // 플레이어 기본 속도
+}
