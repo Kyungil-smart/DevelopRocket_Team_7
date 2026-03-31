7cc21be7b8581cc710dc6d626cf27edbaa688f8e
Byounghee Chae <mybeang@naver.com>
2026-03-31 11:43:41 +0900

    feat: 배터리 획득 로직 추가 및 획득시 Upgrade 기능 추가.

---
+++ ProjectP/Assets/05.SO/Scripts/Item/BatteryStatus.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BatteryData", menuName = "Data/BatteryData")]
+public class BatteryStatus : ScriptableObject
+{
+    [Range(0f, 1f)] public float attackSpeed;
+    [Range(0f, 1f)] public float damage;  
+    [Range(0f, 1f)] public float critRate;
+    [Range(0f, 1f)] public float critMultiplier;
+}
