6ad8cc889f3e92c09dd381e044d7c80c99e75b25
Byounghee Chae <mybeang@naver.com>
2026-04-01 18:28:47 +0900

    feat: tmp controller

---
+++ ProjectP/Assets/02.Scripts/TempController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TempController : MonoBehaviour
+{
+    [SerializeField] private GameObject NodeUI;
+    private bool _isSelected;
+    
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            NodeUI.SetActive(!NodeUI.activeSelf);
+        } 
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Rifle);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Shotgun);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Sniper);
+        }
+    }
+}
