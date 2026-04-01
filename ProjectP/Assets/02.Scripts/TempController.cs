91e53c7544c5d74eec25f35f4a55b06dd86beeb4
Byounghee Chae <mybeang@naver.com>
2026-04-01 18:38:08 +0900

    feat: prototype 에서 간단히 테스트 할 수 있게 코드 추가

--- ProjectP/Assets/02.Scripts/TempController.cs
+++ ProjectP/Assets/02.Scripts/TempController.cs
@@ -1,28 +1,56 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TempController : MonoBehaviour
 {
     [SerializeField] private GameObject NodeUI;
     private bool _isSelected;
-    
-    public void Update()
+    private InputSystem_Actions _actions;
+
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            NodeUI.SetActive(!NodeUI.activeSelf);
-        } 
-        else if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Rifle);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Shotgun);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Sniper);
-        }
+        _actions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        _actions.Enable();
+        _actions.Player.TempNodeUI.started += ChangeNodeUI;
+        _actions.Player.TempRifle.started += SelectRifle;
+        _actions.Player.TempShotgun.started += SelectShotgun;
+        _actions.Player.TempSniper.started += SelectSniper;
+    }
+
+    private void OnDisable()
+    {
+        _actions.Player.TempNodeUI.started -= ChangeNodeUI;
+        _actions.Player.TempRifle.started -= SelectRifle;
+        _actions.Player.TempShotgun.started -= SelectShotgun;
+        _actions.Player.TempSniper.started -= SelectSniper;
+        _actions.Disable();
+    }
+
+    private void ChangeNodeUI(InputAction.CallbackContext context)
+    {
+        NodeUI.SetActive(!NodeUI.activeSelf); 
+    }
+    
+    private void SelectRifle(InputAction.CallbackContext context)
+    {
+        if (!_isSelected) PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Rifle);
+        _isSelected = true;
+    }
+    
+    private void SelectShotgun(InputAction.CallbackContext context)
+    {
+        if (!_isSelected) PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Shotgun);
+        _isSelected = true;
+    }
+    
+    private void SelectSniper(InputAction.CallbackContext context)
+    {
+        if (!_isSelected) PostManager.Instance.Post(PostMessageKey.SelectWeapon, WeaponType.Sniper);
+        _isSelected = true;
     }
 }
