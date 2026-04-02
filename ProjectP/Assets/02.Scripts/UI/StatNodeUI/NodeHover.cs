78cc3d6c102d543c518ba21afb4dacdc2f3e1a44
cminhyeong1999-spec <cminhyeong1999@gmail.com>
2026-04-02 13:10:13 +0900

    feat : 노드 상세정보 호버 기능 추가
    - 노드 이름이랑 증가할 수치를 보여주는 UI 생성
    - 해당 노드 위로 마우스를 올릴 시 UI On, 마우스가 벗어나면 UI Off
    - 폰트 문제 해결

--- ProjectP/Assets/02.Scripts/UI/StatNodeUI/NodeHover.cs
+++ ProjectP/Assets/02.Scripts/UI/StatNodeUI/NodeHover.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -5,11 +6,18 @@ public class NodeHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
 {
     [Header("호버 처리할 부모 오브젝트")]
     [SerializeField] private GameObject _hoverObject;
-    
+
+    public void OnEnable()
+    {
+        // UI 창 켰을 때 호버창이 켜져 있을 경우를 대비하여 off
+        _hoverObject.SetActive(false);
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         // 아이템 정보창 보여주기
         _hoverObject.SetActive(true);
+        _hoverObject.transform.SetAsLastSibling();
     }
 
     public void OnPointerExit(PointerEventData eventData)
