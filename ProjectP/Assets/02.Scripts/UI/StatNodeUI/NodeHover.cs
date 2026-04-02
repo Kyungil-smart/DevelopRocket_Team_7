ea1cbdb340be4058f84af6abd2124c800244e25b
cminhyeong1999-spec <cminhyeong1999@gmail.com>
2026-04-02 12:24:13 +0900

    feat : 노드 호버 기능 추가
    - 현재 폰트를 제대로 불러오지 못하는 현상 확인중

--- ProjectP/Assets/02.Scripts/UI/StatNodeUI/NodeHover.cs
+++ ProjectP/Assets/02.Scripts/UI/StatNodeUI/NodeHover.cs
@@ -3,15 +3,18 @@ using UnityEngine.EventSystems;
 
 public class NodeHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [Header("호버 처리할 부모 오브젝트")]
+    [SerializeField] private GameObject _hoverObject;
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
         // 아이템 정보창 보여주기
-        Debug.Log("마우스 호버");
+        _hoverObject.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         // 정보창 닫기
-        Debug.Log("마우스 나감");
+        _hoverObject.SetActive(false);
     }
 }
