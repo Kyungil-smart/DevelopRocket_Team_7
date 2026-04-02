3c8398b450082ae28d1b7a805c7e924b5dd62673
Byounghee Chae <mybeang@naver.com>
2026-04-02 22:56:06 +0900

    feat: 보스 사망시 포탈 생성 및 포탈 접근시 게임 종료

---
+++ ProjectP/Assets/02.Scripts/Boss/BossRemnant.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BossRemnant : MonoBehaviour
+{
+    [SerializeField] private LayerMask _playerLayer;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("가즈아 가능?");
+        if (Utils.CompareLayer(collision.gameObject.layer, _playerLayer))
+        {
+            Debug.Log("응 가능");
+            PostManager.Instance.Post(PostMessageKey.MainUIGameResult, true);
+        }
+    }
+}
