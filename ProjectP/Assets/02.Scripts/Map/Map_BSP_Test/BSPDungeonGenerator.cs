0c17a8dd15b92d522fc164b43a8f5bbca7938616
SadSmileXD <phycoeco@naver.com>
2026-04-06 17:11:48 +0900

    chore : 머지를 위한 임시 저장

--- ProjectP/Assets/02.Scripts/Map/Map_BSP_Test/BSPDungeonGenerator.cs
+++ ProjectP/Assets/02.Scripts/Map/Map_BSP_Test/BSPDungeonGenerator.cs
@@ -23,7 +23,7 @@ public class BSPDungeonGenerator : MonoBehaviour
     public int mapHeight;
     public int minRoomSize;
     public int maxRoomSize;
-
+    public int MiddleNodeCount;
     [Header("최대 생성할 방의 개수")]
     public int maxRooms;
     //분할 추적하기 위한 카운터
@@ -134,7 +134,7 @@ public class BSPDungeonGenerator : MonoBehaviour
         {
             //  방 상하좌우에 둘 여백(Padding)을 설정합니다.
             // 이 값을 늘릴수록 방 사이의 간격이 넓어집니다. (추천: 2 ~ 3)
-            int padding = 3;
+            int padding = 4;
 
             // 1. 최대 크기 계산 시, 노드 크기에서 양옆/위아래 패딩(padding * 2)만큼 뺍니다.
             int maxW = Mathf.Min(maxRoomSize, node.rect.width - (padding * 2));
@@ -276,7 +276,17 @@ public class BSPDungeonGenerator : MonoBehaviour
             }
             else
             {
-                leafRooms[i].roomType = Random.value > 0.4f ? RoomType.MiddleNode : RoomType.NULL;
+                int count = 0;
+                if(count<MiddleNodeCount)
+                {
+                    count++;
+                    leafRooms[i].roomType= RoomType.MiddleNode;
+                }
+                else
+                {
+                    leafRooms[i].roomType = RoomType.NULL;
+                }
+                //leafRooms[i].roomType = Random.value > 0.4f ? RoomType.MiddleNode : RoomType.NULL;
             }
         }
     }
