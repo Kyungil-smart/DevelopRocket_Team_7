35f7b313af55d00a7c6449eedb3fb3ae64e783e6
Byounghee Chae <mybeang@naver.com>
2026-04-07 11:39:47 +0900

    fix: #142

--- ProjectP/Assets/02.Scripts/Enemy/Actions/EnemyDamaged.cs
+++ ProjectP/Assets/02.Scripts/Enemy/Actions/EnemyDamaged.cs
@@ -37,6 +37,8 @@ public class EnemyDamaged : MonoBehaviour, IDamageable, INeedEnemyBlackboard
 
     public void SetBlackboard(EnemyBlackboard blackboard)
     {
+        if (_effectInDamagedCoroutine != null) StopCoroutine(_effectInDamagedCoroutine);
+        _effectInDamagedCoroutine = null;
         this.blackboard = blackboard;
         _spriteRenderer.color = new Color(1, 1, 1);
     }
